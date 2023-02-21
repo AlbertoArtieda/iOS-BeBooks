d0e8a8979c15244b199cf79632e5102b6a0aee2a
Juan <jaramillocarrilloj25@gmail.com>
2023-02-21 14:31:14 +0100

    Do a pull f

--- iOS-BeBooks/ViewControllers/BookAdditionViewController.swift
+++ iOS-BeBooks/ViewControllers/BookAdditionViewController.swift
@@ -1,35 +1,125 @@
 import UIKit
 
-class BookAdditionViewController: UIViewController {
+class BookAdditionViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
     
     @IBOutlet weak var isbn: UITextField!
     @IBOutlet weak var titulo: UILabel!
     @IBOutlet weak var editoial: UILabel!
     @IBOutlet weak var curso: UILabel!
+    @IBOutlet weak var imagen: UIImageView!
+    var dataToFill: [UILabel] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         isbn.text = "Asdfas"
-        
+        dataToFill = [titulo, curso, editoial]
         // Do any additional setup after loading the view.
     }
     
     @IBAction func fillData(_ sender: UIButton) {
-        // 9788467587142
-        print("algo")
-        let isbn = isbn.text
-        let urlSession = URLSession.shared
-        let url =  URL(string: "https://www.googleapis.com/books/v1/volumes?q=" + isbn!)
-        var recievedItems: Dictionary = [String:Any]()
-        print("print")
-        urlSession.dataTask(with: url!) { data, response, error in
-            print("hola")
-            if let data = data {
-                let json = try? JSONSerialization.jsonObject(with: data)
-                for i in json as! [String: Any]{
+        // 9788467587142 http://127.0.0.1:8000
+        let url =  URL(string:"http://127.0.0.1:8000/getbookinfo")
+
+        let isbn: [String: String] = [
+            "isbn": isbn.text!
+        ]
+        
+        let finalBody = try? JSONSerialization.data(withJSONObject: isbn, options: .prettyPrinted)
+        
+        var request = URLRequest(url: url!)
+        
+        request.httpMethod = "POST"
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        do {
+            request.httpBody = finalBody
+        } catch let error {
+            print("El Error\(error.localizedDescription)")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request){ data, response, error in
+            
+            print("El Response \(String(describing: response))")
+            
+            let json =  try? JSONSerialization.jsonObject(with: data!)
+            
+            if json == nil {
+                print("el resultado ha sido NIL")
+                DispatchQueue.main.async {
+                    let alert = UIAlertController(title: "Error en la búsqueda", message: "No es un libro normalmente utilizado en colegios e institutos", preferredStyle: .alert)
+                    
+                    alert.addAction(UIAlertAction(title: "Aceptar", style: .cancel, handler: {action in}))
+                    
+                    self.present(alert, animated: true)
+                }
+                
+                
+            } else {
+                print("el resultado ha sido EEXITOSO")
+                var filling: [String] = []
+                
+                for i in json as! [String] {
                     print(i)
+                    filling.append(i)
+                }
+                for i in 0...filling.count - 1 {
+                    DispatchQueue.main.async {
+                        self.dataToFill[i].text = filling[i]
+                    }
                 }
             }
         }.resume()
+        
+    }
+    
+    @IBAction func addBookPhoto(_ sender: UIButton) {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {return}
+        imagen.isHidden = false
+        imagen.image = image
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func addBook(_ sender: UIButton) {
+        if imagen.isHidden == false {
+            print("Adelante")
+            let url =  URL(string:"http://127.0.0.1:8000/getbookinfo")
+            
+            //Get data of existing UIImage
+            
+            let libro: [String: String] = [
+                "isbn": isbn.text!,
+                "imagen": "laimagenaqui",
+                "titulo": titulo.text!,
+                "editorial": editoial.text!,
+                "curso": curso.text!
+            ]
+            
+            let finalBody = try? JSONSerialization.data(withJSONObject: libro, options: .prettyPrinted)
+            
+            var request = URLRequest(url: url!)
+            
+            request.httpMethod = "POST"
+            
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+            
+            do {
+                request.httpBody = finalBody
+            } catch let error {
+                print("El Error\(error.localizedDescription)")
+                return
+            }
+        }
     }
 }
--- iOS-BeBooks/ViewControllers/ProfileEditionViewController.swift
+++ iOS-BeBooks/ViewControllers/ProfileEditionViewController.swift
@@ -37,6 +37,54 @@ class ProfileEditionViewController: UIViewController, UIImagePickerControllerDel
         newImg.image = image
         newImg.layer.cornerRadius = 60
         newImg.clipsToBounds = true
+        newImg.isHidden = false
         dismiss(animated: true)
+        let imageData = newImg.image?.jpegData(compressionQuality: 1)
+        // Convert image Data to base64 encoded string
+        let imageBase64String = imageData?.base64EncodedString(options: .lineLength64Characters)
+        print(imageBase64String ?? "Could not encode image to Base64")
+        print(imageBase64String!)
+    }
+    
+    @IBAction func updateData(_ sender: UIButton) {
+        let url =  URL(string:"http://127.0.0.1:8000/newbook")
+        //Get data of existing UIImage
+        let imageData = newImg.image?.jpegData(compressionQuality: 1)
+        // Convert image Data to base64 encodded string
+        let imageBase64String = imageData?.base64EncodedString(options: .lineLength64Characters)
+        print(imageBase64String ?? "Could not encode image to Base64")
+        let imagen: String?
+        if newImg.isHidden {
+            imagen = imageBase64String
+        } else {
+            imagen = ""
+        }
+        
+        let libro: [String: String] = [
+            "imagen": imagen!,
+            "usuario": newUsername.text!,
+            "nombre": newName.text!,
+            "email": newEmail.text!,
+            "direccion": newAddress.text!,
+            "cp": newCP.text!,
+            "provincia": newProvince.text!,
+            "telefono": newPhone.text!,
+        ]
+        
+        let finalBody = try? JSONSerialization.data(withJSONObject: libro, options: .prettyPrinted)
+        
+        var request = URLRequest(url: url!)
+        
+        request.httpMethod = "POST"
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        do {
+            request.httpBody = finalBody
+        } catch let error {
+            print("El Error\(error.localizedDescription)")
+            return
+        }
     }
 }
